6e8869800a4d1103c4b3304a28bdf534baaba385
Angela Daza <angela.daza06@gmail.com>
2024-01-01 19:19:07 -0500

    Se instalan dependencias para compatibilidad de navegadores

--- cafeteria/gulpfile.js
+++ cafeteria/gulpfile.js
@@ -1,11 +1,14 @@
 const { src, dest, watch } = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
+const postcss = require('gulp-postcss');
+const autoprefixer = require('autoprefixer');
 
 function css(done){
     //compilar sas
     //pasos: 1 - identificar archivo, 2 - Compilarla. 3 - Guardar el .css
     src('src/scss/app.scss')
         .pipe( sass() )
+        .pipe(postcss([autoprefixer()]))
         .pipe( dest('build/css') );
 
         done(); //** Indica cuando finaliza el metodo
